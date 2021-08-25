df94dc83f90d397eb6dc6a56dc2d73e8e43a1cf5
Davis Zalitis <davis.zalitis@lattelecom.lv>
2021-08-25 16:12:59 +0300

    cleanup code

--- src/Vairogs/Extra/Constants/HttpStatusCode.php
+++ src/Vairogs/Extra/Constants/HttpStatusCode.php
@@ -70,24 +70,23 @@ final class HttpStatusCode
     public const HTTP_NOT_EXTENDED = Response::HTTP_NOT_EXTENDED;
     public const HTTP_NETWORK_AUTHENTICATION_REQUIRED = Response::HTTP_NETWORK_AUTHENTICATION_REQUIRED;
 
-    /* EXTRA RESPONSES */
-    public const HTTP_RESPONSE_IS_STALE = 110; //
-    public const HTTP_REVALIDATION_FAILED = 111; //
-    public const HTTP_DISCONNECTED_OPERATION = 112; //
-    public const HTTP_HEURISTIC_EXPIRATION = 113; //
-    public const HTTP_MISCELLANEOUS_WARNING = 199; //
-    public const HTTP_THIS_IS_FINE = 218; //
-    public const HTTP_TRANSFORMATION_APPLIED = 214; //
-    public const HTTP_MISCELLANEOUS_PERSISTENT_WARNING = 299; //
-    public const HTTP_ENHANCE_YOUR_CALM = 420; //
-    public const HTTP_CONNECTION_CLOSED_WITHOUT_RESPONSE = 444; //
-    public const HTTP_RETRY_WITH = 449; //
-    public const HTTP_BLOCKED_BY_WINDOWS_PARENTAL_CONTROLS = 450; //
-    public const HTTP_INVALID_TOKEN = 498; //
-    public const HTTP_TOKEN_REQUIRED = 499; //
-    public const HTTP_BANDWIDTH_LIMIT_EXCEEDED = 509; //
-    public const HTTP_SITE_IS_OVERLOADED = 529; //
-    public const HTTP_SITE_IS_FROZEN = 530; //
-    public const HTTP_NETWORK_READ_TIMEOUT_ERROR = 598; //
-    public const HTTP_NETWORK_CONNECT_TIMEOUT_ERROR = 599; //
+    public const HTTP_RESPONSE_IS_STALE = 110;
+    public const HTTP_REVALIDATION_FAILED = 111;
+    public const HTTP_DISCONNECTED_OPERATION = 112;
+    public const HTTP_HEURISTIC_EXPIRATION = 113;
+    public const HTTP_MISCELLANEOUS_WARNING = 199;
+    public const HTTP_THIS_IS_FINE = 218;
+    public const HTTP_TRANSFORMATION_APPLIED = 214;
+    public const HTTP_MISCELLANEOUS_PERSISTENT_WARNING = 299;
+    public const HTTP_ENHANCE_YOUR_CALM = 420;
+    public const HTTP_CONNECTION_CLOSED_WITHOUT_RESPONSE = 444;
+    public const HTTP_RETRY_WITH = 449;
+    public const HTTP_BLOCKED_BY_WINDOWS_PARENTAL_CONTROLS = 450;
+    public const HTTP_INVALID_TOKEN = 498;
+    public const HTTP_TOKEN_REQUIRED = 499;
+    public const HTTP_BANDWIDTH_LIMIT_EXCEEDED = 509;
+    public const HTTP_SITE_IS_OVERLOADED = 529;
+    public const HTTP_SITE_IS_FROZEN = 530;
+    public const HTTP_NETWORK_READ_TIMEOUT_ERROR = 598;
+    public const HTTP_NETWORK_CONNECT_TIMEOUT_ERROR = 599;
 }
