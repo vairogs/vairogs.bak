b42198a8c1f54befd5c3bbf54e9b45b76cd7d691
Davis Zalitis <davis.zalitis@lattelecom.lv>
2021-08-31 14:26:48 +0300

    check if class exists before initializing

--- src/Vairogs/Component/Utils/Doctrine/Model/Factory/LogArrayFactory.php
+++ src/Vairogs/Component/Utils/Doctrine/Model/Factory/LogArrayFactory.php
@@ -4,15 +4,20 @@ namespace Vairogs\Component\Utils\Doctrine\Model\Factory;
 
 use InvalidArgumentException;
 use Vairogs\Component\Utils\Doctrine\Model\Log;
+use function class_exists;
 use function sprintf;
 
 class LogArrayFactory
 {
     public static function create(array $data, string $class = Log::class): Log
     {
+        if (!class_exists($class)) {
+            throw new InvalidArgumentException(sprintf('Class % does not exist', $class));
+        }
+
         $object = new $class();
         if (!$object instanceof Log) {
-            throw new InvalidArgumentException(sprintf('%s class must extend %s', $class, Log::class));
+            throw new InvalidArgumentException(sprintf('Class %s must extend %s', $class, Log::class));
         }
 
         return $object
