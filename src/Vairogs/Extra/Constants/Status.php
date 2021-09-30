edcc66afce227c2e34298cc496187c14235886e1
Davis Zalitis <davis.zalitis@lattelecom.lv>
2021-09-30 18:38:15 +0300

    add status constants

---
+++ src/Vairogs/Extra/Constants/Status.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types = 1);
+
+namespace Vairogs\Extra\Constants;
+
+final class Status
+{
+    public const ENABLED = 'enabled';
+    public const DISABLED = 'disabled';
+    public const Y = 'y';
+    public const N = 'n';
+    public const YES = 'yes';
+    public const NO = 'no';
+    public const ON = 'on';
+    public const OFF = 'off';
+    public const ONE = 1;
+    public const ZERO = 0;
+    public const SUCCESS = 'success';
+    public const ERROR = 'error';
+}
