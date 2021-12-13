631fbf3670b5d1875ef53adc46b9b327c509cd04
k0d3r1s <k0d3r1s@gmail.com>
2021-12-13 14:47:09 +0200

    add function to build request array (for new logging purposes)

--- src/Vairogs/Utils/Helper/Util.php
+++ src/Vairogs/Utils/Helper/Util.php
@@ -2,8 +2,10 @@
 
 namespace Vairogs\Utils\Helper;
 
+use Symfony\Component\HttpFoundation\Request;
 use Vairogs\Utils\Twig\Annotation;
 use function array_fill;
+use function file_get_contents;
 use function implode;
 use function preg_match;
 
@@ -15,5 +17,18 @@ final class Util
         preg_match(pattern: '#^1?$|^(11+?)\1+$#', subject: implode(separator: '1', array: array_fill(start_index: 0, count: $number, value: null)), matches: $matches);
 
         return isset($matches[1]);
+    }
+
+    #[Annotation\TwigFunction]
+    #[Annotation\TwigFilter]
+    public static function getRequestIdentity(Request $request): array
+    {
+        $requestData = Uri::buildArrayFromObject($request);
+        $additionalData = [
+            'actualIp' => file_get_contents('https://ident.me'),
+            'uuid' => time() . Identification::getUniqueId(),
+        ];
+
+        return array_merge($requestData, $additionalData);
     }
 }
