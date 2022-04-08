df8502eae47809ca827060c5c4073bf86edb6bcf
k0d3r1s <k0d3r1s@gmail.com>
2022-04-08 06:30:15 +0300

    init tests

--- src/Vairogs/Utils/Helper/Util.php
+++ src/Vairogs/Utils/Helper/Util.php
@@ -6,17 +6,48 @@ use Symfony\Component\HttpFoundation\Request;
 use Vairogs\Utils\Twig\Attribute;
 use function array_fill;
 use function file_get_contents;
+use function function_exists;
 use function implode;
 use function preg_match;
 
 final class Util
 {
     #[Attribute\TwigFunction]
+    #[Attribute\TwigFilter]
     public static function isPrime(int $number): bool
     {
+        if (null !== $prime = self::isPrimeGmp(number: (string) $number)) {
+            return $prime;
+        }
+
+        if (null !== $prime = self::isPrimeBelow1000(number: $number)) {
+            return $prime;
+        }
+
         preg_match(pattern: '#^1?$|^(11+?)\1+$#', subject: implode(separator: '1', array: array_fill(start_index: 0, count: $number, value: null)), matches: $matches);
 
         return isset($matches[1]);
+    }
+
+    #[Attribute\TwigFunction]
+    #[Attribute\TwigFilter]
+    public static function isPrimeBelow1000(int $number): ?bool
+    {
+        if (1000 <= $number) {
+            return null;
+        }
+
+        if (1 === $number) {
+            return false;
+        }
+
+        for ($x = 2; $x < $number; $x++) {
+            if (0 === $number % $x) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     #[Attribute\TwigFunction]
@@ -29,5 +60,18 @@ final class Util
         ];
 
         return array_merge(Uri::buildArrayFromObject(object: $request), $additionalData);
+    }
+
+    private static function isPrimeGmp(string $number): ?bool
+    {
+        if (function_exists(function: 'gmp_prob_prime')) {
+            return match (gmp_prob_prime(num: $number)) {
+                0 => false,
+                2 => true,
+                default => null
+            };
+        }
+
+        return null;
     }
 }
---
+++ tests/Utils/Helper/UtilTest.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types = 1);
+
+namespace Vairogs\Tests\Utils\Helper;
+
+use PHPUnit\Framework\TestCase;
+use Vairogs\Utils\Helper\Util;
+
+class UtilTest extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testIsPrime(int $a, bool $expected, ?bool $expectedBelow): void
+    {
+        $this->assertSame(expected: $expected, actual: Util::isPrime(number: $a));
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testIsPrimeBelow1000(int $a, bool $expected, ?bool $expectedBelow): void
+    {
+        $this->assertSame(expected: $expectedBelow, actual: Util::isPrimeBelow1000(number: $a));
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            [2,    true,   true],
+            [3,    true,   true],
+            [5,    true,   true],
+            [43,   true,   true],
+            [367,  true,   true],
+            [3019, true,   null],
+            [1,    false, false],
+            [4,    false, false],
+            [10,   false, false],
+            [32,   false, false],
+            [360,  false, false],
+            [3182, false,  null],
+        ];
+    }
+}
