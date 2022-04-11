a8e4bc20761f60df7879097ce50e2027a48f797b
k0d3r1s <k0d3r1s@gmail.com>
2022-04-11 17:12:58 +0300

    split latvian sort functions to new class

--- src/Vairogs/Utils/Helper/Sort.php
+++ src/Vairogs/Utils/Helper/Sort.php
@@ -4,10 +4,8 @@ namespace Vairogs\Utils\Helper;
 
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\Criteria;
-use Exception;
 use InvalidArgumentException;
 use JetBrains\PhpStorm\Pure;
-use Vairogs\Extra\Constants\Symbol;
 use Vairogs\Utils\Twig\Attribute;
 use function array_key_exists;
 use function array_slice;
@@ -25,8 +23,6 @@ use function usort;
 /** @noinspection TypoSafeNamingInspection */
 final class Sort
 {
-    private static string $field = '';
-
     #[Attribute\TwigFunction]
     public static function swap(mixed &$foo, mixed &$bar): void
     {
@@ -141,63 +137,6 @@ final class Sort
 
             return -1 * $flip;
         };
-    }
-
-    #[Attribute\TwigFilter]
-    public static function sortLatvian(array &$names, string $field, array $callback = [self::class, 'compareLatvian']): bool
-    {
-        self::$field = $field;
-        $result = usort(array: $names, callback: $callback);
-        self::$field = '';
-
-        return $result;
-    }
-
-    public static function compareLatvianArray(array $first, array $second): int
-    {
-        if (null === ($firstValue = $first[self::$field] ?? null) || null === ($secondValue = $second[self::$field] ?? null)) {
-            return 0;
-        }
-
-        return self::compare(first: $firstValue, second: $secondValue);
-    }
-
-    public static function compareLatvian(array|object $first, array|object $second): int
-    {
-        return match (true) {
-            is_array(value: $first) && is_array(value: $second) => self::compareLatvianArray(first: $first, second: $second),
-            is_object(value: $first) && is_object(value: $second) => self::compareLatvianObject(first: $first, second: $second),
-            default => 0
-        };
-    }
-
-    public static function compareLatvianObject(object $first, object $second): int
-    {
-        try {
-            $firstValue = Php::hijackGet(object: $first, property: self::$field);
-            $secondValue = Php::hijackGet(object: $second, property: self::$field);
-        } catch (Exception) {
-            return 0;
-        }
-
-        return self::compare(first: $firstValue, second: $secondValue);
-    }
-
-    private static function compare(string $first, string $second): int
-    {
-        for ($i = 0, $len = mb_strlen(string: $first); $i < $len; $i++) {
-            if (mb_substr(string: $first, start: $i, length: 1) === mb_substr(string: $second, start: $i, length: 1)) {
-                continue;
-            }
-
-            if ($i > mb_strlen(string: $second) || mb_strpos(haystack: Symbol::LV_LOWERCASE, needle: mb_substr(string: $first, start: $i, length: 1)) > mb_strpos(haystack: Symbol::LV_LOWERCASE, needle: mb_substr(string: $second, start: $i, length: 1))) {
-                return 1;
-            }
-
-            return -1;
-        }
-
-        return 0;
     }
 
     private static function merge(array $left, array $right): array
---
+++ src/Vairogs/Utils/Helper/SortLatvian.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types = 1);
+
+namespace Vairogs\Utils\Helper;
+
+use Exception;
+use Vairogs\Extra\Constants\Symbol;
+use Vairogs\Utils\Twig\Attribute;
+use function is_array;
+use function is_object;
+use function usort;
+
+class SortLatvian
+{
+    private static string $field = '';
+
+    #[Attribute\TwigFilter]
+    public static function sortLatvian(array &$names, string $field, array $callback = [self::class, 'compareLatvian']): bool
+    {
+        self::$field = $field;
+        $result = usort(array: $names, callback: $callback);
+        self::$field = '';
+
+        return $result;
+    }
+
+    public static function compareLatvianArray(array $first, array $second): int
+    {
+        if (null === ($firstValue = $first[self::$field] ?? null) || null === ($secondValue = $second[self::$field] ?? null)) {
+            return 0;
+        }
+
+        return self::compare(first: $firstValue, second: $secondValue);
+    }
+
+    public static function compareLatvian(array|object $first, array|object $second): int
+    {
+        return match (true) {
+            is_array(value: $first) && is_array(value: $second) => self::compareLatvianArray(first: $first, second: $second),
+            is_object(value: $first) && is_object(value: $second) => self::compareLatvianObject(first: $first, second: $second),
+            default => 0
+        };
+    }
+
+    public static function compareLatvianObject(object $first, object $second): int
+    {
+        try {
+            $firstValue = Php::hijackGet(object: $first, property: self::$field);
+            $secondValue = Php::hijackGet(object: $second, property: self::$field);
+        } catch (Exception) {
+            return 0;
+        }
+
+        return self::compare(first: $firstValue, second: $secondValue);
+    }
+
+    private static function compare(string $first, string $second): int
+    {
+        for ($i = 0, $len = mb_strlen(string: $first); $i < $len; $i++) {
+            if (mb_substr(string: $first, start: $i, length: 1) === mb_substr(string: $second, start: $i, length: 1)) {
+                continue;
+            }
+
+            if ($i > mb_strlen(string: $second) || mb_strpos(haystack: Symbol::LV_LOWERCASE, needle: mb_substr(string: $first, start: $i, length: 1)) > mb_strpos(haystack: Symbol::LV_LOWERCASE, needle: mb_substr(string: $second, start: $i, length: 1))) {
+                return 1;
+            }
+
+            return -1;
+        }
+
+        return 0;
+    }
+}
