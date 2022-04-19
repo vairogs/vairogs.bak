ebb6129b2a7209b729c62525cbcdd16d58eebbc1
k0d3r1s <k0d3r1s@gmail.com>
2022-04-19 17:00:39 +0300

    fix duplication

--- src/Vairogs/Cache/Adapter/AbstractRedisAdapter.php
+++ src/Vairogs/Cache/Adapter/AbstractRedisAdapter.php
@@ -8,15 +8,19 @@ use Redis;
 use Symfony\Component\Cache\Adapter\RedisAdapter;
 use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Vairogs\Core\Vairogs;
+use Vairogs\Utils\Helper\Composer;
+use function implode;
+use function sprintf;
 
 abstract class AbstractRedisAdapter implements Adapter
 {
-    protected const MESSAGE = 'In order to use %s, package(s)/extension(s) "%s" must be installed';
+    protected string $class;
+    protected array $packages;
 
     public function __construct(protected readonly Redis|ClientInterface $client, protected readonly string $namespace = Vairogs::VAIROGS)
     {
-        if (null !== $message = $this->checkDeclaration()) {
-            throw new InvalidConfigurationException(message: $message);
+        if (!Composer::isInstalled(packages: $this->packages, includeDevRequirements: false)) {
+            throw new InvalidConfigurationException(message: sprintf('In order to use %s, package(s)/extension(s) "%s" must be installed', $this->class, implode(separator: ',', array: $this->packages)));
         }
     }
 
@@ -24,6 +28,4 @@ abstract class AbstractRedisAdapter implements Adapter
     {
         return new RedisAdapter(redis: $this->client, namespace: $this->namespace, defaultLifetime: Adapter::DEFAULT_LIFETIME);
     }
-
-    abstract protected function checkDeclaration(): ?string;
 }
--- src/Vairogs/Cache/Adapter/Predis.php
+++ src/Vairogs/Cache/Adapter/Predis.php
@@ -2,19 +2,8 @@
 
 namespace Vairogs\Cache\Adapter;
 
-use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
-use Vairogs\Utils\Helper\Composer;
-use function implode;
-use function sprintf;
-
 final class Predis extends AbstractRedisAdapter
 {
-    protected function checkDeclaration(): ?string
-    {
-        if (!Composer::isInstalled(packages: $packages = ['predis/predis'], includeDevRequirements: false)) {
-            throw new InvalidConfigurationException(message: sprintf(self::MESSAGE, self::class, implode(separator: ',', array: $packages)));
-        }
-
-        return null;
-    }
+    protected string $class = self::class;
+    protected array $packages = ['predis/predis'];
 }
