887c68b9f54acceac7896a39c19a5216f96d03a0
k0d3r1s <k0d3r1s@gmail.com>
2022-04-24 01:14:18 +0300

    sort php-cs-fixer config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,64 +4,41 @@ if (!file_exists(filename: __DIR__ . '/src')) {
     exit;
 }
 
-return (new PhpCsFixer\Config())->setRules(rules: [
-    '@PHP80Migration:risky' => true,
-    '@PHP81Migration' => true,
-    '@Symfony' => true,
-    '@Symfony:risky' => true,
-    '@PSR12' => true,
-    '@PSR12:risky' => true,
-    'protected_to_private' => false,
-    'nullable_type_declaration_for_default_null_value' => ['use_nullable_type_declaration' => true],
-    'declare_strict_types' => true,
-    'linebreak_after_opening_tag' => false,
-    'blank_line_after_opening_tag' => false,
-    'binary_operator_spaces' => true,
-    'concat_space' => ['spacing' => 'one'],
-    'declare_equal_normalize' => ['space' => 'single'],
-    'yoda_style' => true,
-    'native_function_invocation' => false,
-    'native_constant_invocation' => false,
-    'fopen_flags' => [
-        'b_mode' => true,
-    ],
-    'increment_style' => [
-        'style' => 'post',
-    ],
-    'comment_to_phpdoc' => true,
-    'types_spaces' => [
-        'space' => 'none',
-    ],
-    'native_function_type_declaration_casing' => true,
-    'magic_constant_casing' => true,
-    'ordered_imports' => [
-        'sort_algorithm' => 'alpha',
-        'imports_order' => [
-            'class',
-            'function',
-            'const',
-        ],
-    ],
-    'multiline_whitespace_before_semicolons' => [
-        'strategy' => 'no_multi_line',
-    ],
-    'method_chaining_indentation' => false,
-    'return_assignment' => true,
-    'ordered_class_elements' => [
-        'sort_algorithm' => 'none',
-    ],
-    'simple_to_complex_string_variable' => true,
-    'explicit_indirect_variable' => true,
-    'no_superfluous_phpdoc_tags' => [
-        'allow_mixed' => true,
-        'remove_inheritdoc' => true,
-    ],
-    'no_useless_sprintf' => true,
-    'global_namespace_import' => [
-        'import_constants' => null,
-        'import_functions' => true,
-        'import_classes' => true,
-    ],
-])
+return (new PhpCsFixer\Config())
+    ->setRules(rules: [
+        '@PHP80Migration:risky' => true,
+        '@PHP81Migration' => true,
+        '@PSR12' => true,
+        '@PSR12:risky' => true,
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'binary_operator_spaces' => true,
+        'blank_line_after_opening_tag' => false,
+        'comment_to_phpdoc' => true,
+        'concat_space' => ['spacing' => 'one'],
+        'declare_equal_normalize' => ['space' => 'single'],
+        'declare_strict_types' => true,
+        'explicit_indirect_variable' => true,
+        'fopen_flags' => [ 'b_mode' => true,],
+        'global_namespace_import' => ['import_constants' => null, 'import_functions' => true, 'import_classes' => true,],
+        'increment_style' => ['style' => 'post',],
+        'linebreak_after_opening_tag' => false,
+        'magic_constant_casing' => true,
+        'method_chaining_indentation' => false,
+        'multiline_whitespace_before_semicolons' => ['strategy' => 'no_multi_line',],
+        'native_constant_invocation' => false,
+        'native_function_invocation' => false,
+        'native_function_type_declaration_casing' => true,
+        'no_superfluous_phpdoc_tags' => ['allow_mixed' => true, 'remove_inheritdoc' => true,],
+        'no_useless_sprintf' => true,
+        'nullable_type_declaration_for_default_null_value' => ['use_nullable_type_declaration' => true],
+        'ordered_class_elements' => ['sort_algorithm' => 'none',],
+        'ordered_imports' => ['sort_algorithm' => 'alpha', 'imports_order' => ['class', 'function', 'const',],],
+        'protected_to_private' => false,
+        'return_assignment' => true,
+        'simple_to_complex_string_variable' => true,
+        'types_spaces' => ['space' => 'none',],
+        'yoda_style' => true,
+    ])
     ->setRiskyAllowed(isRiskyAllowed: true)
     ->setCacheFile(cacheFile: '.php-cs-fixer.cache');
