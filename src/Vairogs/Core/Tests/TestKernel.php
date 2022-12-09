ff74ee58d67b08fe3bbdf8c6bbfa1f66620c69de
k0d3r1s <k0d3r1s@gmail.com>
2022-12-09 16:54:08 +0200

    wip: rework tests: remove unused code

--- src/Vairogs/Core/Tests/TestKernel.php
+++ src/Vairogs/Core/Tests/TestKernel.php
@@ -2,13 +2,8 @@
 
 namespace Vairogs\Core\Tests;
 
-use ReflectionObject;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 use function getcwd;
 use function hash;
@@ -25,41 +20,11 @@ class TestKernel extends Kernel
 
     public function getCacheDir(): string
     {
-        /* @noinspection ProjectDirParameter */
         return sys_get_temp_dir() . '/Vairogs/' . hash(algo: 'xxh128', data: getcwd()) . '/var/cache';
     }
 
     protected function getConfigDir(): string
     {
-        /* @noinspection ProjectDirParameter */
         return $this->getProjectDir() . '/Tests/Resources/config';
-    }
-
-    protected function getBundlesPath(): string
-    {
-        return $this->getConfigDir() . '/bundles.php';
-    }
-
-    /** @noinspection PhpUnusedParameterInspection */
-    protected function configureContainer(ContainerConfigurator $container, LoaderInterface $loader, ContainerBuilder $builder): void
-    {
-        $configDir = $this->getConfigDir();
-
-        $container->import(resource: $configDir . '/{packages}/*.{php}');
-        $container->import(resource: $configDir . '/{packages}/' . $this->getEnvironment() . '/*.{php}');
-        $container->import(resource: $configDir . '/{services}.php');
-    }
-
-    protected function configureRoutes(RoutingConfigurator $routes): void
-    {
-        $configDir = $this->getConfigDir();
-
-        $routes->import(resource: $configDir . '/{routes}/' . $this->getEnvironment() . '/*.{php}');
-        $routes->import(resource: $configDir . '/{routes}/*.{php}');
-        $routes->import(resource: $configDir . '/{routes}.php');
-
-        if (false !== ($fileName = (new ReflectionObject($this))->getFileName())) {
-            $routes->import(resource: $fileName, type: 'attribute');
-        }
     }
 }
--- src/Vairogs/Core/Tests/VairogsTestCase.php
+++ src/Vairogs/Core/Tests/VairogsTestCase.php
@@ -6,65 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Vairogs\Core\Vairogs;
 
-use function array_map;
-use function flock;
-use function fopen;
-use function glob;
-use function is_dir;
-use function rmdir;
 use function sys_get_temp_dir;
-use function touch;
-use function unlink;
 
 use const DIRECTORY_SEPARATOR;
-use const GLOB_NOSORT;
-use const LOCK_EX;
-use const LOCK_NB;
-use const LOCK_SH;
 
 abstract class VairogsTestCase extends KernelTestCase
 {
-    final protected const LOCKFILE = 'vairogs-test-initialization-lock-file';
     protected ContainerInterface $container;
     protected string $directory;
-    private static bool $initialized = false;
 
     protected function setUp(): void
     {
-        $tmp = sys_get_temp_dir();
-
-        if (!self::$initialized) {
-            touch(filename: $tmpFile = $tmp . DIRECTORY_SEPARATOR . self::LOCKFILE);
-            $lockFile = fopen(filename: $tmpFile, mode: 'rb');
-
-            if (!flock(stream: $lockFile, operation: LOCK_EX | LOCK_NB)) {
-                flock(stream: $lockFile, operation: LOCK_SH);
-            }
-
-            self::$initialized = true;
-        }
-
-        $this->directory = $tmp . DIRECTORY_SEPARATOR . Vairogs::VAIROGS . '-test';
-
-        foreach ([$this->directory, $tmp . DIRECTORY_SEPARATOR . Vairogs::VAIROGS] as $dir) {
-            if (is_dir(filename: $dir)) {
-                $this->rmdir(directory: $dir);
-            }
-        }
-
+        $this->directory = sys_get_temp_dir() . DIRECTORY_SEPARATOR . Vairogs::VAIROGS . '-test';
         $this->container = static::getContainer();
-    }
-
-    protected function tearDown(): void
-    {
-        unlink(filename: sys_get_temp_dir() . DIRECTORY_SEPARATOR . self::LOCKFILE);
-        self::$initialized = false;
-    }
-
-    protected function rmdir(string $directory): bool
-    {
-        array_map(callback: fn (string $file) => is_dir(filename: $file) ? $this->rmdir(directory: $file) : unlink(filename: $file), array: glob(pattern: $directory . '/*', flags: GLOB_NOSORT));
-
-        return !is_dir(filename: $directory) || rmdir(directory: $directory);
     }
 }
